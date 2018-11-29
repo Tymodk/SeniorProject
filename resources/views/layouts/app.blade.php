bd14bff0b2b94b22aa351ef6661212051cbc1275
arthurvanpassel <32160634+arthurvanpassel@users.noreply.github.com>
2018-11-29 15:40:01 +0100

    15:39

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,11 @@
                                 @endif
                             </li>
                         @else
+                            @if (Auth::user()->name == 'admin')
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('admin.home') }}">{{ __('Admin page') }}</a>
+                            </li>
+                            @endif
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
