8cbb8ce70a6581c3a7e9c64b724f76ec5fe9358e
arthurvanpassel <32160634+arthurvanpassel@users.noreply.github.com>
2018-12-13 10:53:59 +0100

    home wireframe

--- resources/views/wireframe.blade.php
+++ resources/views/wireframe.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-    <div class="container-fluid justify-content-center text-center mt-5" style="border-bottom: 1px solid black">
+    <div class="container-fluid justify-content-center text-center mt-5 border-bottom border-dark">
         <div class="row">
             <div class="col-md-4">
         <h2 class="text-center pt-3">Welcome Sam!</h2>
@@ -12,34 +12,124 @@
             <h2 class="text-center"> <p>14:22 </p>  <p>17/12/2018</p> </h2>
             </div>
             <div class="col-md-4">
-            <h2 class="pull-right p-4" style="border: 1px solid black">No scannner detected</h2>
+            <h2 class="pull-right p-4 border border-dark">No scannner detected</h2>
             </div>
             </div>
     </div>
+    <div class="container-fluid">
+      <div class="container-fluid row mt-2 border-bottom border-dark pb-3">
+        <p><strong>Today</strong></p>
         <div class="container-fluid">
-          <div class="container row mt-2">
-            <p><strong>Today</strong></p>
-            <div class="container">
-        		<!-- start agenda -->
-          		<div >
-                <div class="row">
-                  <div class="col-md-2">
-                    9u - 10u
-                  </div>
-                  <div class="col-md-3">
-                    Senior Project
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-md-2">
-                    9u - 10u
-                  </div>
-                  <div class="col-md-3">
-                    Senior Project
-                  </div>
-                </div>
-        	</div>
-
-
-        </div>
+    		<!-- start agenda -->
+          <div class="row mt-2 vertical-align-text border-bottom border-dark">
+            <div class="col-md-2">
+              <h5>9u - 10u</h5>
+            </div>
+            <div class="col-md-3">
+              <h3>Senior Project</h3>
+            </div>
+            <div class="col-md-2">
+              <h5>A102</h5>
+            </div>
+            <div class="col-md-3">
+              <h5>IWT/PB-MT 3 WP a</h5>
+            </div>
+            <div class="col-md-2 p-1 border mb-2 text-center ">
+              <h5 class="m-0">Start Lesson</h5>
+            </div>
+          </div>
+          <div class="row mt-2 vertical-align-text border-bottom border-dark">
+            <div class="col-md-2">
+              <h5>10u30 - 13u</h5>
+            </div>
+            <div class="col-md-3">
+              <h3>Web Development</h3>
+            </div>
+            <div class="col-md-2">
+              <h5>B108</h5>
+            </div>
+            <div class="col-md-3">
+              <h5>IWT/PB-MT 3 WP a</h5>
+            </div>
+            <div class="col-md-2 p-1 border mb-2 text-center ">
+              <h5 class="m-0">Start Lesson</h5>
+            </div>
+          </div>
+          <div class="row mt-2 vertical-align-text border-bottom border-dark">
+            <div class="col-md-2">
+              <h5>15u15 - 17</h5>
+            </div>
+            <div class="col-md-3">
+              <h3>Web Research</h3>
+            </div>
+            <div class="col-md-2">
+              <h5>A106</h5>
+            </div>
+            <div class="col-md-3">
+              <h5>IWT/PB-MT 3 WP a</h5>
+            </div>
+            <div class="col-md-2 p-1 border mb-2 text-center ">
+              <h5 class="m-0">Start Lesson</h5>
+            </div>
+          </div>
+      	</div>
+      </div>
+      <div class="container-fluid row mt-2 border-bottom border-dark pb-3">
+        <p><strong>Today</strong></p>
+        <div class="container-fluid">
+    		<!-- start agenda -->
+          <div class="row mt-2 vertical-align-text border-bottom border-dark">
+            <div class="col-md-2">
+              <h5>9u - 10u</h5>
+            </div>
+            <div class="col-md-3">
+              <h3>Senior Project</h3>
+            </div>
+            <div class="col-md-2">
+              <h5>A102</h5>
+            </div>
+            <div class="col-md-3">
+              <h5>IWT/PB-MT 3 WP a</h5>
+            </div>
+            <div class="col-md-2 p-1 border mb-2 text-center ">
+              <h5 class="m-0">Start Lesson</h5>
+            </div>
+          </div>
+          <div class="row mt-2 vertical-align-text border-bottom border-dark">
+            <div class="col-md-2">
+              <h5>10u30 - 13u</h5>
+            </div>
+            <div class="col-md-3">
+              <h3>Web Development</h3>
+            </div>
+            <div class="col-md-2">
+              <h5>B108</h5>
+            </div>
+            <div class="col-md-3">
+              <h5>IWT/PB-MT 3 WP a</h5>
+            </div>
+            <div class="col-md-2 p-1 border mb-2 text-center ">
+              <h5 class="m-0">Start Lesson</h5>
+            </div>
+          </div>
+          <div class="row mt-2 vertical-align-text border-bottom border-dark">
+            <div class="col-md-2">
+              <h5>15u15 - 17</h5>
+            </div>
+            <div class="col-md-3">
+              <h3>Web Research</h3>
+            </div>
+            <div class="col-md-2">
+              <h5>A106</h5>
+            </div>
+            <div class="col-md-3">
+              <h5>IWT/PB-MT 3 WP a</h5>
+            </div>
+            <div class="col-md-2 p-1 border mb-2 text-center ">
+              <h5 class="m-0">Start Lesson</h5>
+            </div>
+          </div>
+      	</div>
+      </div>
+    </div>
     @endsection
